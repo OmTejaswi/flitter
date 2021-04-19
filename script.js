b3fecc93d7569e3ceae50aa6e3fe554c09f2be9d
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-04-19 08:14:53 +0530

    updated

--- script.js
+++ script.js
@@ -8,16 +8,56 @@ db.ref("playerCount").on("value",function(data){
     pc = data.val();
 })
 
-function btnPressed(){
-    pc += 1;
-    
-    var user = document.getElementById("input").value;
-    localStorage.setItem("user",user);
 
-    db.ref("users/user"+pc).update({
-        user: username,
-    })
-    db.ref("/").update({
-        playerCount: pc,
-    })
+
+
+
+//variables
+var names = [];
+var limit = 0;
+
+function setup(){
+    
+}
+function draw(){
+    if(limit === 0 ) {
+        for(var i = 1; i < pc+1; i++) {
+            db.ref("users/user"+i+"/name").on("value",function(data){
+                names.push(data.val());
+            })
+            limit = 1;
+        }
+    }
+   console.log(names);
+}
+
+function btnPressed(){
+    var user = document.getElementById("input").value;
+    if(names[0] === undefined) {
+        pc += 1;
+        localStorage.setItem("user",user);
+
+        db.ref("users/user"+pc).update({
+            name: user,
+        })
+        db.ref("/").update({
+            playerCount: pc,
+        })
+}
+    for(var i = 0; i < names.length; i++) {
+        if(document.getElementById("input").value !== names[i]) {
+            pc += 1;
+            localStorage.setItem("user",user);
+    
+            db.ref("users/user"+pc).update({
+                name: user,
+            })
+            db.ref("/").update({
+                playerCount: pc,
+            })
+            names.push(user)
+        }
+        
+    } 
+    
 }
