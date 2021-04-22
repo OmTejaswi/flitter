426b4373d5c76ba4a12a00f45040006837277786
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-04-22 17:23:08 +0530

    updated

--- rooms/script.js
+++ rooms/script.js
@@ -1,10 +1,29 @@
 var user = localStorage.getItem("user");
-var greet
+var greet;
+var db = firebase.database();
 
-function setup() {
-    //greet = createElement("h2").html("Welcome " +user+ " !");
     document.getElementById("greet").innerHTML = "Welcome " +user+ " !";
+    document.getElementById("addroom").innerHTML = "#Add Room";
+
+    var pc;
+    db.ref("playerCount").on("value",function(data){
+        pc = data.val();
+    });
+    var rc;
+    db.ref("roomCount").on("value",function(data){
+        rc = data.val();
+    })
+
+function addroom(){
+    if(document.getElementById("room_name") !== "" &&
+        document.getElementById("room_password") !== "") {
+            db.ref("rooms/"+document.getElementById("room_name").value).update({
+                room: "#"+document.getElementById("room_name").value,
+                password: document.getElementById("room_password").value,
+            })
+        }
 }
+
 function logout(){
     localStorage.removeItem("user");
     window.location.replace("../")
--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//variables
+var names = [];
+var limit = 0;
+
+function draw(){
+    if(limit === 0 ) {
+        for(var i = 1; i < pc+1; i++) {
+            db.ref("users/user"+i+"/name").on("value",function(data){
+                names.push(data.val());
+            })
+            limit = 1;
+        }
+    }
+}
+
+setInterval(function(){
+    if(names.includes(document.getElementById("input").value.toLowerCase()) === true) {
+        localStorage.setItem("user",document.getElementById("input").value);
+        window.location.replace("./rooms")
+    }
+},1000);
+
 var username = localStorage.getItem("user");
 document.getElementById("input").value = username; 
 
@@ -9,59 +31,25 @@ db.ref("playerCount").on("value",function(data){
 })
 
 
-
-
-
-//variables
-var names = [];
-var limit = 0;
-
-function setup(){
-    
-}
-function draw(){
-    if(limit === 0 ) {
-        for(var i = 1; i < pc+1; i++) {
-            db.ref("users/user"+i+"/name").on("value",function(data){
-                names.push(data.val());
-            })
-            limit = 1;
-        }
-    }
-    if(names.includes(document.getElementById("input").value.toLowerCase()) === true) {
-        window.location.replace("./rooms")
-    }
-}
-
 function btnPressed(){
     var user = document.getElementById("input").value;
-    if(names[0] === undefined) {
-        pc += 1;
-        localStorage.setItem("user",user);
-
-        db.ref("users/user"+pc).update({
-            name: user,
-        })
-        db.ref("/").update({
-            playerCount: pc,
-        })
-        window.location.replace("./rooms")
-}
-    if(names.includes(document.getElementById("input").value.toLowerCase()) === false) {
-            pc += 1;
-            localStorage.setItem("user",user);
-    
-            db.ref("users/user"+pc).update({
-                name: user,
-            })
-            db.ref("/").update({
-                playerCount: pc,
-            })
-            names.push(user)
+    if(user !== "") {
+        if(names.includes(document.getElementById("input").value.toLowerCase()) === false) {
+                pc += 1;
+                db.ref("/").update({
+                    playerCount: pc,
+                })
+                localStorage.setItem("user",user);
+                db.ref("users/user"+pc).update({
+                    name: user,
+                })
+                names.push(user);
+                window.location.replace("./rooms")
+        }
+        if(names.includes(document.getElementById("input").value.toLowerCase()) === true) {
+            console.error("Name Already Exists")
             window.location.replace("./rooms")
-    }
-    if(names.includes(document.getElementById("input").value.toLowerCase()) === true) {
-        console.error("Name Already Exists")
+        }
     }
     
 }
